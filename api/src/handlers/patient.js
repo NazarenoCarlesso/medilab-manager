93602950fd8ed40ca0a348ea2ba8e5dd71de990c
sdelp66 <sdelp66@gmail.com>
2023-02-27 17:31:38 -0300

    handler patient  sign up

--- api/src/handlers/patient.js
+++ api/src/handlers/patient.js
@@ -14,4 +14,45 @@ const patientLogInHandler = async (req, res) => {
     res.status(200).json(patient)
 }
 
-module.exports = { patientAllHandler, patientLogInHandler }
+const patientSignUpHandler = async (req, res) => {
+    const { username, password, email, firstName, lastName, dni, number, sex, height, civilState } = req.body
+
+    const newPatient = await patientSignUpHandler(username, password, email, firstName, lastName, dni, number, sex, height, civilState)
+
+    res.status(200).json(newPatient)
+}
+
+module.exports = { patientAllHandler, patientLogInHandler, patientSignUpHandler }
+
+/**password: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING(25),
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING(25),
+            allowNull: false
+        },
+        dni: {
+            type: DataTypes.STRING(25),
+            allowNull: true
+        },
+        number: {
+            type: DataTypes.STRING(25),
+            allowNull: true
+        },
+        sex: {
+            type: DataTypes.STRING(1),
+            allowNull: true
+        },
+        height: {
+            type: DataTypes.FLOAT,
+            allowNull: true
+        },
+        civilState: {
+            type: DataTypes.STRING(25),
+            allowNull: true
+        }, */
