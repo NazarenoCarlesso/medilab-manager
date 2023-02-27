a857a9eb86c10867c3b4a795859a94fd5e92c2a4
sdelp66 <sdelp66@gmail.com>
2023-02-27 20:21:28 -0300

    solucion del conflicto de mails duplicados

--- api/src/controllers/patient.js
+++ api/src/controllers/patient.js
@@ -27,42 +27,57 @@ const patientLogIn = async (username, password) => {
     return { id, firstName, lastName }
 }
 
-const patientSignUp = async (username, password, email, firstName, lastName, dni, number, sex, height, civilState) => {
-    try {
-      // Generar una contraseña encriptada con bcrypt
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      // Crear un nuevo objeto de paciente con la contraseña encriptada
-      const newPatient = {
-        username,
-        email,
-        password: hashedPassword,
-        firstName,
-        lastName,
-        dni,
-        number,
-        sex,
-        height,
-        civilState
-      };
-  
-      // Crear el registro de paciente en la base de datos
-      const newPatientCreated = await Patient.create(newPatient);
-      console.log('Paciente creado:', newPatientCreated.toJSON());
-  
-      return newPatientCreated;
-    } catch (error) {
-      console.error('Error al crear paciente:', error);
-      throw error;
-    }
-  };
+// con chequeo de duplicidad de mail...
 
-// const patientSignUp = async (username, password, email, firstName, lastName, dni, number, sex, height, civilState) =>{
-    
-//     const newPatient = {
+const patientSignUp = async (username, password, email, firstName, lastName, dni, number, sex, height, civilState) => {
+try {
+    // Buscar si ya existe un paciente con el correo electrónico proporcionado
+    const existingPatient = await Patient.findOne({ where: { email } });
+
+
+    if (existingPatient) {
+        return { error: 'El correo electrónico ya está en uso' };
+    }
+
+    // Generar una contraseña encriptada con bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crear un nuevo objeto de paciente con la contraseña encriptada
+    const newPatient = {
+    username,
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName,
+    dni,
+    number,
+    sex,
+    height,
+    civilState
+    };
+
+    // Crear el registro de paciente en la base de datos
+    const newPatientCreated = await Patient.create(newPatient);
+    console.log('Paciente creado:', newPatientCreated.toJSON());
+
+    return newPatientCreated;
+} catch (error) {
+    console.error('Error al crear paciente:', error);
+    throw error;
+}
+};
+
+
+// const patientSignUp = async (username, password, email, firstName, lastName, dni, number, sex, height, civilState) => {
+//     try {
+//       // Generar una contraseña encriptada con bcrypt
+//       const hashedPassword = await bcrypt.hash(password, 10);
+  
+//       // Crear un nuevo objeto de paciente con la contraseña encriptada
+//       const newPatient = {
 //         username,
 //         email,
-//         password,
+//         password: hashedPassword,
 //         firstName,
 //         lastName,
 //         dni,
@@ -71,20 +86,17 @@ const patientSignUp = async (username, password, email, firstName, lastName, dni
 //         height,
 //         civilState
 //       };
-      
-//     const newPatientCreated = await Patient.create(newPatient)
-//         .then(patient => {
-//           console.log('Paciente creado:', patient.toJSON());
-//         })
-//         .catch(error => {
-//           console.error('Error al crear paciente:', error);
-//         });
-    
-    
-    
+  
+//       // Crear el registro de paciente en la base de datos
+//       const newPatientCreated = await Patient.create(newPatient);
+//       console.log('Paciente creado:', newPatientCreated.toJSON());
+  
+//       return newPatientCreated;
+//     } catch (error) {
+//       console.error('Error al crear paciente:', error);
+//       throw error;
+//     }
+//   };
 
-//     return newPatientCreated;
-
-// }
 
 module.exports = { patientAll, patientLogIn, patientSignUp }
--- api/src/handlers/patient.js
+++ api/src/handlers/patient.js
@@ -15,12 +15,31 @@ const patientLogInHandler = async (req, res) => {
 }
 
 const patientSignUpHandler = async (req, res) => {
+    try {
+      const { username, password, email, firstName, lastName, dni, number, sex, height, civilState } = req.body;
+      const newPatient = await patientSignUp(username, password, email, firstName, lastName, dni, number, sex, height, civilState);
+  
+      if (newPatient.error) {
+        return res.status(409).json({ error: newPatient.error });
+      }
+  
+      return res.status(201).json(newPatient);
+    } catch (error) {
+      console.error('Error al crear paciente:', error);
+      return res.status(500).json({ error: 'Error al crear paciente' });
+    }
+  };
+  
+  
+  
 
-    const { username, password, email, firstName, lastName, dni, number, sex, height, civilState } = req.body;
+// const patientSignUpHandler = async (req, res) => {
 
-    const newPatient = await patientSignUp(username, password, email, firstName, lastName, dni, number, sex, height, civilState)
+//     const { username, password, email, firstName, lastName, dni, number, sex, height, civilState } = req.body;
 
-    res.status(200).json(newPatient)
-}
+//     const newPatient = await patientSignUp(username, password, email, firstName, lastName, dni, number, sex, height, civilState)
+
+//     res.status(200).json(newPatient)
+// }
 
 module.exports = { patientAllHandler, patientLogInHandler, patientSignUpHandler }
