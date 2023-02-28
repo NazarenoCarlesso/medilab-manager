f3019a6787353073bd24d861511b71e43759cec0
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-28 18:59:40 -0300

    rutas de pacientes validadas con express-validator

--- api/src/routes/patient.js
+++ api/src/routes/patient.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 
 // middlewares
+const { check, header } = require('express-validator')
 const validateJWT = require('../middlewares/validateJWT')
+const validateReq = require('../middlewares/validateReq')
 
 // handlers
 const {
@@ -16,10 +18,31 @@ const router = Router()
 
 router.get('/', patientAllHandler)
 
-router.post('/login', patientLogInHandler)
+router.post('/login', [
+    check('username', 'Username es obligatorio').not().isEmpty(),
+    check('password', 'Contraseña es obligatoria').not().isEmpty(),
+    validateReq
+], patientLogInHandler)
 
-router.post('/signup', patientSignUpHandler)
+router.post('/signup', [
+    check('username', 'Username es obligatorio').not().isEmpty(),
+    check('password', 'Contraseña es obligatoria').not().isEmpty(),
+    check('email', 'Email es obligatorio').not().isEmpty(),
+    check('firstName', 'Nombre es obligatorio').not().isEmpty(),
+    check('lastName', 'Apellido es obligatorio').not().isEmpty(),
+    check('email', 'Email debe ser válido').isEmail(),
+    check('username', 'Username debe ser de 8 a 25 caracteres de largo').isLength({ min: 8, max: 25 }),
+    check('password', 'Contraseña debe ser de 8 a 25 caracteres de largo').isLength({ min: 8, max: 25 }),
+    check('firstName', 'Nombre debe ser de 8 a 25 caracteres de largo').isLength({ min: 8, max: 25 }),
+    check('lastName', 'Apellido debe ser de 8 a 25 caracteres de largo').isLength({ min: 8, max: 25 }),
+    check('sex', 'Sexo solo puede contener un caracter').isLength({ min: 0, max: 1 }),
+    validateReq
+], patientSignUpHandler)
 
-router.delete('/:id', validateJWT, patientDeleteHandler)
+router.delete('/:id', [
+    header('token', 'Token es obligatorio').not().isEmpty(),
+    validateReq,
+    validateJWT
+], patientDeleteHandler)
 
 module.exports = router
