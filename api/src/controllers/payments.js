2a2c9a1e38a80127499267b5736e4be759408749
sdelp66 <sdelp66@gmail.com>
2023-03-06 17:49:32 -0300

    funciona get payments

--- api/src/controllers/payments.js
+++ api/src/controllers/payments.js
@@ -5,17 +5,18 @@ const { QueryTypes } = require('sequelize')
 // conexion con Sequelize
 const { models } = require('../db.js')
 const { sequelize } = require('../db.js')
-const { Payment } = models
+const { Payment, Order } = models
 
 
 const paymentsById = async (PatientId) => {
-    const payments = await sequelize.query(
-        `SELECT p.*
-        FROM public."Payments" p
-        JOIN public."Orders" o ON p.id = o."PaymentId"
-        WHERE o."PatientId" = ${PatientId};`,
-        { type: QueryTypes.SELECT }
-    );
+
+    const payments = await Payment.findAll({
+        include: [{
+          model: Order,
+          where: { PatientId }
+        }]
+      });
+      
     
     return payments;
 }
