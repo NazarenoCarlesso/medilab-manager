ed653b2fb340734d8cca78bfeae65b082c383151
IvanAlfredoQQ <ivan.alfredo.q.q@gmail.com>
2023-03-10 23:11:25 -0300

    Visual Changes

--- client/src/components/UserRoutes/Profile.jsx
+++ client/src/components/UserRoutes/Profile.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import styles from "./Profile.module.css"
+import React, { useEffect } from "react";
+import styles from "./Profile.module.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import img from "../../images/logo.png"
+const BACK = process.env.REACT_APP_BACK;
+
 
 export default function Profile() {
+  const token = useSelector((state) => state.sessionId?.token);
+
+  const getMe = async () => {
+    try {
+      const config = {
+        headers: { token:`${token}`},
+      };
+      const response = await axios.get(`${BACK}/users/me`,config);
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  
+  useEffect(() => {
+    getMe()
+  },[])
+
   return (
-    <div className={styles.ZonaDeDatos} title="Zona de Datos">
-        <div className={styles.UserCard} title="Datos paciente">
-          <div className={styles.AvatarContainer}>
-            <div className={styles.AvatarPhoto}>
-              <img alt="AvatarIMG"></img>
-            </div>
+    <div className={styles.UserCard}>
+      <div className={styles.Container}>
+        <div className={styles.Banner}>
+          MEDILAB - AFILIADO
+        </div>
+        <div className={styles.Information}>
+          <div className={styles.Avatar}><div className={styles.AvatarPhoto}><img src={img} alt="ProfilePicture"></img></div></div>
+          <div className={styles.UserInfo}>
+            <p>Nombre</p>
+            <p>Edad</p>
+            <p>DNI</p>
+            <p>Numero</p>
           </div>
-          
-          <div className={styles.UserInfo}>Info</div>
-          <div></div>
-        </div> 
+        </div>
       </div>
-  )
+    </div>
+  );
 }
