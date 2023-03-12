c6b4dbd46a8f224e93e3427eb30cf1c85df453b4
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-03-11 21:24:51 -0300

    categories and samples admin dash

--- client/src/components/SearchUI.jsx
+++ client/src/components/SearchUI.jsx
@@ -6,7 +6,10 @@ import PaginationItem from '@mui/material/PaginationItem';
 import Stack from '@mui/material/Stack';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { FormControl, FormHelperText, Paper, TextField } from '@mui/material';
+import { Box, FormControl, FormHelperText, Paper, TextField } from '@mui/material';
+import ProfileUI from './ProfileUI';
+import CategoriesUI from './CategoriesUI';
+import SamplesUI from './SamplesUI';
 
 const BACK = process.env.REACT_APP_BACK
 
@@ -28,60 +31,43 @@ export default function TestsUI() {
     useEffect(() => setPage(1), [search])
 
     return (
-            <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
-                <Paper style={{ width: 360 }}>
-                    <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
-                        <FormControl sx={{ m: 1, width: 340 }}>
-                            <TextField value={search} onChange={e => setSearch(e.target.value)} id="outlined-search" label="Search field" type="search" />
-                            <FormHelperText>With label + helper text</FormHelperText>
-                        </FormControl>
-                        {/*<FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-helper-label">Categoria</InputLabel>
-                        <Select labelId="demo-simple-select-helper-label" id="demo-simple-select-helper" label="Age"
-                            value={category} onChange={e => setCategory(e.target.value)}>
-                            <MenuItem value=""><em>Ninguna</em> </MenuItem>
-                            {categories.map(c => <MenuItem value={c}>{c}</MenuItem>)}
-                        </Select>
+        <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
+            <Paper style={{ width: 360 }}>
+                <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
+                    <FormControl sx={{ m: 1, width: 340 }}>
+                        <TextField value={search} onChange={e => setSearch(e.target.value)} id="outlined-search" label="Search field" type="search" />
                         <FormHelperText>With label + helper text</FormHelperText>
                     </FormControl>
-                    
-                    <FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-helper-label">Muestra</InputLabel>
-                        <Select labelId="demo-simple-select-helper-label" id="demo-simple-select-helper" label="Age"
-                            value={sample} onChange={e => setSample(e.target.value)}>
-                            <MenuItem value=""><em>Ninguno</em></MenuItem>
-                            {samples.map(s => <MenuItem value={s}>{s}</MenuItem>)}
-                        </Select>
-                        <FormHelperText>With label + helper text</FormHelperText>
-                    </FormControl>
-                    <FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <Button variant="contained">Contained</Button>
-                        <FormHelperText>With label + helper text</FormHelperText>
-                    </FormControl>*/}
-                        <Stack spacing={2}>
-                            <Pagination
-                                count={Math.round(count / 5)}
-                                page={page}
-                                onChange={(e, v) => setPage(v)}
-                                renderItem={(item) => <PaginationItem
-                                    slots={{
-                                        previous: ArrowBackIcon,
-                                        next: ArrowForwardIcon
-                                    }}
-                                    {...item} />}
-                            />
-                        </Stack>
-                    </Grid>
-                </Paper>
-                <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
-                    {tests.map(test => <TestUI
-                        key={test.id}
-                        id={test.id}
-                        name={test.name}
-                        description={test.description}
-                        price={test.price}
-                    />)}
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={Math.round(count / 5)}
+                            page={page}
+                            onChange={(e, v) => setPage(v)}
+                            renderItem={(item) => <PaginationItem
+                                slots={{
+                                    previous: ArrowBackIcon,
+                                    next: ArrowForwardIcon
+                                }}
+                                {...item} />}
+                        />
+                    </Stack>
                 </Grid>
+            </Paper>
+            <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
+                {tests.map(test => <TestUI
+                    key={test.id}
+                    id={test.id}
+                    name={test.name}
+                    description={test.description}
+                    price={test.price}
+                />)}
             </Grid>
+            <Grid container direction="row" justifyContent="center" alignItems="start">
+                <Box><ProfileUI /></Box>
+                <Box><CategoriesUI /></Box>
+                <Box><SamplesUI /></Box>
+            </Grid>
+            <div style={{ height: 200 }} />
+        </Grid>
     )
 }
