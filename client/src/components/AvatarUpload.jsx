70a41ae1008342805fecfd222606f3966ac512b7
IvanAlfredoQQ <ivan.alfredo.q.q@gmail.com>
2023-03-17 22:07:03 -0300

    Funcionando

--- client/src/components/AvatarUpload.jsx
+++ client/src/components/AvatarUpload.jsx
@@ -15,29 +15,23 @@ const [fileName, setFileName] = useState("Archivo sin seleccionar");
 const token = useSelector(state => state.token)
 
 const uploadHandler = (e)=>{
-    setFile(e.target)
-    const uploadedFile = e.target.files[0] //[0] because it's meant to be unique
-    setFileName(uploadedFile.name)
-    if(uploadedFile){
-        setImage(URL.createObjectURL(uploadedFile))
-    }
+  //If an image is loaded, this array will be != 0, else the user clicked cancel in upload
+  if(e.target.files.length !== 0){
+  const uploadedFile = e.target.files[0] //[0] because it's meant to be only 1 file in the array
+  setFile(uploadedFile)
+  setFileName(uploadedFile.name)
+  setImage(URL.createObjectURL(uploadedFile))    
+  }
+  else{}
 }
 
 const handleSubmit = async (e)=>{
 
     e.preventDefault() //Prevent page reload because of submit behaviour
 
-    console.log("THIS IS FILE STATE (event.target)", file)
-    console.log("THIS IS FILE STATE (event.target.files) THIS ONE IS SEND TO BACK", file.files) //This is the one i should append
-    console.log("THIS IS FILE STATE (event.target.files[0])", file.files[0])
-    console.log("THIS IS FILENAME STATE (event.target.files[0].name)", fileName)
+    const formData = new FormData();  //multipart/form-data
+    formData.append("archivo", file)
 
-    //const imageFile = document.querySelector('.inputfield').files  //Trying to obtain the input from DOM
-    //console.log("THIS IS QUERY SELECTOR OF IMAGEFILE",imageFile)  //This is the same as (event.target.files)
-    
-    const formData = new FormData();
-    formData.append(fileName, file.files)
-    
     const posted = await axios.post(`${BACK}/users/photo`, formData, {
         headers: {
           'token': token,
@@ -45,7 +39,12 @@ const handleSubmit = async (e)=>{
         }
       }
     )
-    console.log(posted)
+    if(posted.status === 201){
+      setFileName("CARGA DE IMAGEN EXITOSA")
+      setTimeout(()=>{window.location.reload()},5000)
+    }else{
+      setFileName("HUBO UN ERROR EN LA CARGA DE LA IMAGEN")
+    }
 }
 
 
@@ -55,15 +54,15 @@ const handleSubmit = async (e)=>{
             <input type="file" accept=".jpg, .png" className='inputfield' hidden
             onChange={(e)=>{uploadHandler(e)}}></input>
             {image ?
-            <img src={image} height={320} alt={fileName}/> 
-            : <><CloudUploadIcon fontSize="large"/> <p>Haz click para subir un archivo</p></>}
+            <img className='image' src={image} alt={fileName}/> 
+            : <><CloudUploadIcon fontSize="large"/> <h5>Haz click para subir un archivo</h5></>}
         </form>
         <section className='UploadedRow'>
             <FilePresentIcon />
-            <span>{fileName}</span>
-            <div className='IconsContainer'>
-                <div className='TrashIcon' onClick={()=>{setFileName("Archivo sin seleccionar"); setImage(null) }} ><DeleteIcon/></div>
-                <button type='submit' form='myForm' className='SendIcon'><SendIcon/></button>
+            <span >{fileName}</span>
+            <div hidden={file? false: true} className='IconsContainer'>
+                <div className='TrashIcon' onClick={()=>{setFileName("Archivo sin seleccionar"); setImage(null); setFile(null) }} ><DeleteIcon/></div>
+                <button type='submit' form='myForm' className='SendIcon'  ><SendIcon/></button>
             </div>
         </section>
     </div>
