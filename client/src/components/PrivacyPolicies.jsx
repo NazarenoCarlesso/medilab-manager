d71486b3974e7c969a6c6a586c98e001d26f1318
Edwin <ing.eliasincio123@gmail.com>
2023-03-19 14:27:07 -0500

    Creacion del componente de politicas de privacidad

---
+++ client/src/components/PrivacyPolicies.jsx
@@ -0,0 +1,347 @@
+import React from "react";
+import styles from "./PrivacyPolicies.module.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default function PrivacyPolicies() {
+  return (
+    <div className="container">
+      <div className={styles.privacyTitle}>Políticas de Privacidad</div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Clases de Datos recogidos</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Entre las clases de Datos Personales que esta Web recoge, ya sea
+          directamente o a través de terceros, se encuentran: Formulario de
+          delivery y agendamiento: tipo de documento de identidad, número de
+          documento, fecha de nacimiento, correo electrónico, teléfono móvil,
+          dirección de toma de muestra. Formulario de registro de médicos:
+          nombres, apellidos, tipo de colegiatura (CMP, COP, Otro), número de
+          colegiatura, especialidad, teléfono, correo electrónico, definición de
+          resultados automáticos, centro de trabajo, dirección. Libro de
+          reclamaciones: fecha de reclamación, tipo de documento de identidad,
+          número de documento de identidad, nombres, apellidos, correo
+          electrónico, teléfono, tipo de reclamación, fecha de incidente, hora
+          de incidente, sede, código de atención, descripción. Formulario de
+          postulación: Fecha, Nombre, Apellido(s), tipo de documento de
+          identidad, número de documento de identidad,Dirección de correo
+          electrónico, curriculum vitae, cargo al que postula, comentarios.
+        </p>
+        <p>
+          Datos. Los Datos Personales pueden ser proporcionados por el Usuario
+          por su propia voluntad, o bien ser recogidos automáticamente durante
+          la utilización de esta Web. Esta página utiliza Cookies. Por defecto
+          las Cookies están activadas. El Usuario puede desactivar todas las
+          Cookies no esenciales para el funcionamiento de esta Web haciendo clic
+          aquí El Usuario asume la responsabilidad por los Datos Personales de
+          terceros compartidos mediante esta Web y declara que tiene derecho a
+          comunicarlos, liberando al Responsable de Tratamiento de toda
+          responsabilidad al respecto.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Modalidades de Tratamiento</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          El Responsable de Tratamiento tratará los Datos de los Usuarios de
+          manera adecuada y adoptará las medidas de seguridad apropiadas para
+          impedir el acceso, la revelación, alteración o destrucción no
+          autorizados de los Datos.
+        </p>
+        <p>
+          El tratamiento de Datos se realiza mediante ordenadores y/o
+          herramientas informáticas, siguiendo procedimientos y modalidades
+          organizativas estrictamente relacionadas con las finalidades
+          señaladas. Además del Responsable de Tratamiento, en algunos casos
+          podrán acceder a los Datos ciertas categorías de personas relacionadas
+          con el funcionamiento de la página (administración, ventas, marketing,
+          departamento jurídico y de administración de sistemas) o contratistas
+          externos que presten servicios al Responsable de Tratamiento (tales
+          como proveedores externos de servicios técnicos, empresas de
+          mensajería, “hosting providers”, empresas de informática, agencias de
+          comunicación) que serán nombrados por el Titular como Encargados del
+          Tratamiento, si fuera necesario. Se podrá solicitar al Responsable de
+          Tratamiento en cualquier momento una lista actualizada de dichas
+          personas.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Lugar</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Los Datos serán tratados en la sede operativa del Responsable de
+          Tratamiento, así como en otros lugares en los que se encuentren
+          situadas las partes que también estén involucradas en dicho
+          tratamiento. Para más información, por favor póngase en contacto con
+          el Responsable de Tratamiento.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Período de Conservación</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Los Datos serán tratados durante el plazo necesario para prestar el
+          servicio solicitado por el Usuario, o el que se requiera en función de
+          las finalidades descritas en este documento, y el Usuario tendrá en
+          todo momento la facultad de solicitar la interrupción de su
+          Tratamiento o la cancelación de los Datos.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Finalidad del Tratamiento de los Datos recogidos
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Los Datos relativos al Usuario son recogidos para permitir la
+          prestación de los servicios propios de la Web, así como para las
+          siguientes finalidades: Contactar con el Usuario, Interacción con
+          redes sociales y plataformas externas y Estadísticas. Los Datos
+          Personales utilizados para cada finalidad son descritos en los
+          apartados específicos de este documento.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Información detallada del tratamiento de los Datos Personales
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Se recogen Datos Personales para las siguientes finalidades y
+          utilizando los siguientes servicios: Contactar con el Usuario,
+          permitir prestar el servicio solicitado por el Usuario, Estadísticas,
+          entender cómo el visitante utiliza el sitio web, entender qué
+          dispositivo usa para acceder al sitio web, optimizar el contenido del
+          sitio web, mostrar anuncios relevantes para el usuario individual e
+          Interacción con redes sociales y plataformas externas.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Defensa jurídica</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Los Datos Personales del Usuario podrán ser utilizados para la defensa
+          jurídica del Propietario en juicio o en las fases prejudiciales
+          previas a un posible pleito derivado del uso abusivo por parte del
+          Usuario de esta Web o de los servicios relacionados.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Información adicional acerca de los Datos Personales del Usuario
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Además de las informaciones contenidas en esta política de privacidad,
+          esta Web podrá proporcionar al Usuario información contextual relativa
+          a servicios específicos o a la recogida y tratamiento de los Datos
+          Personales.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Log del sistema y mantenimiento
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Por motivos relativos al funcionamiento y el mantenimiento, esta Web y
+          cualesquiera otros servicios de terceros que se utilicen podrán
+          recoger un Log del sistema, esto es, archivos que registren las
+          interacciones y que pueden contener Datos Personales, tales como la
+          dirección IP del Usuario.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Información no contenida en esta política de privacidad
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Se podrá solicitar en cualquier momento información adicional sobre el
+          tratamiento de los Datos Personales al Responsable de Tratamiento.
+          Encontrará la información de contacto al final del presente documento.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Ejercicio de los derechos por los Titulares de los Datos
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Los titulares a los que se refieren los Datos Personales tienen
+          derecho a obtener en cualquier momento la confirmación de que estos
+          han sido almacenados por el Responsable de Tratamiento, a conocer su
+          contenido y origen, a verificar su exactitud o solicitar que sean
+          completados, cancelados, actualizados o rectificados, a que sean
+          anonimizados o a que se bloqueen aquellos Datos Personales que están
+          siendo tratados en contravención de las leyes, así como a oponerse a
+          su tratamiento por cualquier motivo legítimo. Las solicitudes deberán
+          remitirse al Responsable de Tratamiento utilizando los datos de
+          contacto indicados.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Derecho de acceso / información
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Tienes derecho a ser informado sobre tus datos personales incluidos en
+          nuestra base de datos, la forma que fueron recopilados, las razones
+          que motivaron su recopilación y a solicitud de quien se realizó la
+          recopilación, las transferencias realizadas, así como de las
+          condiciones y uso que les damos. Te brindaremos toda la información
+          sobre la finalidad para la cual serán utilizados sus datos personales,
+          quiénes tienen acceso a ellos, en cuáles de nuestras base de datos se
+          almacenarán, así como nuestra identidad y domicilio como titulares de
+          la base de datos y, de ser el caso, del encargado de gestionar sus
+          datos personales, de la transferencias nacionales o internacionales
+          que hacemos de los mismos, del carácter obligatorio o facultativo de
+          las respuestas a los cuestionarios que entreguemos, de las
+          consecuencias de proporcionar sus datos personales y de su negativa a
+          hacerlo, del tiempo de conservación de los mismos y de la posibilidad
+          de ejercer los derechos que las normas te conceden y los medios
+          previstos para ello.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Derecho de rectificación / actualización
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Puedes solicitar la actualización, inclusión o modificación de tus
+          datos personales cuando tus datos sean parcial o totalmente inexactos,
+          incompletos, erróneos, falsos o desactualizados. Deberás especificar
+          los datos que deseas que sean rectificados/ actualizados/ modificados
+          / incluidos así como la corrección/incorporación que quieras que la
+          Empresa realice y adjuntar los documentos de sustento aplicables.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square">
+            {" "}
+            Derecho de cancelación / supresión
+          </i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Puedes solicitar la supresión o cancelación total o parcial de tus
+          datos personales de nuestra base de datos si han dejado de ser
+          necesarios para la finalidad que fueron recopilados, si venció el
+          plazo para su tratamiento o el tratamiento no se realice conforme a
+          las normas de protección de datos personales. Ten en cuenta que tu
+          solicitud de cancelación no procederá si mantienes una relación
+          contractual con “La Empresa” ni y tus datos sean necesarios para
+          ejecutar dicha relación contractual. Tampoco procederá cuando estemos
+          obligados a conservar tus datos personales durante los plazos
+          previstos en las normas vigentes ni cuando sean conservados en virtud
+          de razones históricas, estadísticas o científicas de acuerdo con la
+          legislación aplicable.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Derecho de oposición</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          Tienes derecho a oponerte a figurar en nuestra base de datos
+          personales o a la gestión de la Información cuando no hayas autorizado
+          su recopilación, por haber sido tomada de fuentes de acceso al público
+          o cuando habiendo prestado tu consentimiento, acredite la existencia
+          de motivos fundados y legítimos relativos a una concreta situación
+          personal que justifique el ejercicio de este derecho.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Derecho de revocación</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          En cualquier momento y sin justificación previa nos puedes pedir que
+          tus datos personales dejen de ser utilizados para alguna de las
+          finalidades previamente autorizadas. Ten en consideración que tu
+          solicitud de revocación no procederá si mantienes una relación
+          contractual con La Empresa y tus datos son necesarios para ejecutar
+          dicha relación contractual. Tampoco procederá cuando estemos obligados
+          a conservar tus datos personales durante los plazos previstos en las
+          normas vigentes ni cuando sean conservados por razones históricas,
+          estadísticas o científicas de acuerdo legislación aplicable.
+        </p>
+      </div>
+      <div>
+        <h4>
+          <i className="bi bi-check2-square"> Modificación de esta política de privacidad</i>
+        </h4>
+      </div>
+      <div>
+        <p>
+          El Responsable de Tratamiento se reserva el derecho de modificar esta
+          política de privacidad en cualquier momento notificándolo a los
+          Usuarios a través de esta página. Se recomienda encarecidamente que
+          revisen esta página con frecuencia, tomando como referencia la fecha
+          de la última modificación indicada al final. En el caso de que un
+          Usuario esté en desacuerdo con alguna de las modificaciones realizadas
+          a esta Política, el Usuario deberá terminar en el uso de esta Web y
+          podrá solicitar al Responsable de Tratamiento que elimine sus Datos
+          Personales. Salvo que se indique lo contrario, la política de
+          privacidad vigente en cada momento será de aplicación a todos los
+          Datos Personales que el Responsable de Tratamiento haya recogido hasta
+          entonces.
+        </p>
+      </div>
+    </div>
+  );
+}
