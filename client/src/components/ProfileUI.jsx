bdf2c2432e12f4d19436d0665f31f145aae98a54
IvanAlfredoQQ <ivan.alfredo.q.q@gmail.com>
2023-03-22 11:07:36 -0300

    Finalizado el componente de carga de imagen

--- client/src/components/ProfileUI.jsx
+++ client/src/components/ProfileUI.jsx
@@ -9,6 +9,7 @@ import FemaleIcon from '@mui/icons-material/Female'
 import PolicyIcon from '@mui/icons-material/Policy'
 import ChurchIcon from '@mui/icons-material/Church'
 import VerifiedUserIcon from '@mui/icons-material/VerifiedUser'
+import EditIcon from '@mui/icons-material/Edit'
 import FingerprintIcon from '@mui/icons-material/Fingerprint'
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled'
 import AvatarUpload from "./AvatarUpload"
@@ -25,6 +26,7 @@ const Typo = styled(Typography)(({ theme }) => ({
 export default function ProfileUI() {
     const token = useSelector(state => state.token)
     const name = useSelector(state => state.name)
+    const avatar = useSelector(state => state.avatar)
 
     const [user, setUser] = useState({})
 
@@ -36,16 +38,22 @@ export default function ProfileUI() {
         fetch(`${BACK}/users/me`, { headers: { 'token': token } })
             .then(response => response.json())
             .then(data => setUser(data))
-    }, [token])
+    }, [token,avatar])
 
     return (
         <Paper sx={{ height: 340, padding: '10px', margin: 0.5, boxShadow: '0px 0px 10px 0px #00000047' }}>
             <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
-                <Avatar alt={user.name} referrerPolicy="no-referrer" src={user.photo} sx={{ width: 100, height: 100 }} onClick={handleOpen} />
+                <div className="ProfileAvatarContainer">
+                    <Avatar alt={user.name} referrerPolicy="no-referrer" src={user.photo} sx={{ width: 100, height: 100 }}/>
+                    <div className="SelectAvatar"
+                        onClick={handleOpen} 
+                        onMouseOver={()=> document.getElementById("selectedAvatar").hidden = false}
+                        onMouseOut={()=> document.getElementById("selectedAvatar").hidden = true}><EditIcon id="selectedAvatar" hidden={true} sx={{fontSize:50, color: "#5080FD"}}/></div>
+                </div>
                 <Modal className="ModalAvatar" open={open} onClose={handleClose}>
-                    <Paper className="AvatarUI" sx={{background:"#4EC4CE"}}>
+                    <Paper className="AvatarUI" sx={{background:"#FFFFFF"}}>
                             <Grid container justifyContent="space-evenly" alignItems="center" >
-                                <AvatarUpload></AvatarUpload>
+                                <AvatarUpload handleClose={handleClose} ></AvatarUpload>
                             </Grid>
                     </Paper>
                 </Modal>
