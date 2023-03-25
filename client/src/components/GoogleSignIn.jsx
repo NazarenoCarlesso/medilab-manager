e76721cabd9162bb60c6650814925312194bc3de
fabianbm15 <cmelgarejobm@gmail.com>
2023-03-24 19:17:18 -0500

    cambios en carrito de compras

--- client/src/components/GoogleSignIn.jsx
+++ client/src/components/GoogleSignIn.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { setToken, setName, setAvatar, setRole } from '../reducer'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setToken, setName, setAvatar, setRole } from "../reducer";
 
-const BACK = process.env.REACT_APP_BACK
+const BACK = process.env.REACT_APP_BACK;
 
 /*
 const userData = {
@@ -16,53 +16,60 @@ const userData = {
   navigate("/user");
 */
 
-export default function GoogleSignIn() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+export default function GoogleSignIn(props) {
+   const { setShowAlertLogin, fromCart } = props;
 
-    const handleCredentialResponse = ({ credential }) => {
-        fetch(`${BACK}/users/google`, {
-            method: 'post',
-            headers: {
-                'token': credential
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+   const handleCredentialResponse = ({ credential }) => {
+      fetch(`${BACK}/users/google`, {
+         method: "post",
+         headers: {
+            token: credential,
+         },
+      })
+         .then(async (response) => ({
+            token: response.headers.get("token"),
+            body: await response.json(),
+         }))
+         .then((data) => {
+            dispatch(setToken(data.token));
+            dispatch(setName(data.body.name));
+            dispatch(setAvatar(data.body.avatar));
+            dispatch(setRole(data.body.role));
+         })
+         .then(() => {
+            if (fromCart === true) {
+               setShowAlertLogin(false);
+               navigate("/cart");
+            } else {
+               navigate("/dashboard");
             }
-        })
-            .then(async response => ({
-                token: response.headers.get('token'),
-                body: await response.json()
-            }))
-            .then(data => {
-                dispatch(setToken(data.token))
-                dispatch(setName(data.body.name))
-                dispatch(setAvatar(data.body.avatar))
-                dispatch(setRole(data.body.role))
-            })
-            .then(() => navigate('/home'))
-    }
+         });
+   };
 
-    useEffect(() => {
-        /* global google */
-        if (window.google) {
-            google.accounts.id.initialize({
-                client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-                callback: handleCredentialResponse
-            })
+   useEffect(() => {
+      /* global google */
+      if (window.google) {
+         google.accounts.id.initialize({
+            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            callback: handleCredentialResponse,
+         });
 
-            google.accounts.id.renderButton(document.getElementById('googleSignIn'), {
-                // customization
-                type: 'standard',
-                theme: 'filled_blue',
-                size: 'large',
-                text: 'signin_with',
-                shape: 'pill',
-                logo_alignment: 'left',
-                locale: 'es_ES'
-            })
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+         google.accounts.id.renderButton(document.getElementById("googleSignIn"), {
+            // customization
+            type: "standard",
+            theme: "filled_blue",
+            size: "large",
+            text: "signin_with",
+            shape: "pill",
+            logo_alignment: "left",
+            locale: "es_ES",
+         });
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
-    return (
-        <div id='googleSignIn' />
-    )
+   return <div id="googleSignIn" />;
 }
